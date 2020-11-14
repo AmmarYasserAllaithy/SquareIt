255e4098dd98737f3a93fd84fd8ae375f07bd70e
Ammar Yasser AllaiThy <AmmarYasserAllaiThy@gmail.com>
2020-11-14 07:07:46 +0200

    Add toInt()

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,6 +4,8 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
+import static app.utils.toInt;
+
 public class Main {
 
     public static void main(String[] args) throws IOException {
@@ -12,14 +14,11 @@ public class Main {
         if (!(args.length == 3 || args.length == 5)) throw new IllegalArgumentException(
                 "Illegal Arguments...\n image padding color ratio[Width Height][optional]");
 
-        squareIt
-                .setImageFile(new File(args[0]))
-                .setPadding(Integer.parseInt(args[1]))
+        squareIt.setImageFile(new File(args[0]))
+                .setPadding(toInt(args[1]))
                 .setColor(args[2]);
 
-        if (args.length == 5) squareIt.setRatio(new Dimension(
-                Integer.parseInt(args[3]),
-                Integer.parseInt(args[4])));
+        if (args.length == 5) squareIt.setRatio(new Dimension(toInt(args[3]), toInt(args[4])));
 
         boolean written = squareIt.start();
 
